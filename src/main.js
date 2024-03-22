a849e1b5ede3a53ee90aa1e76a07c3812b435d3b
Ain Zaffry <azaffry17@gmail.com>
2024-03-22 08:04:00 +0800

    Version 1

--- src/main.js
+++ src/main.js
@@ -6,19 +6,18 @@ import {
 
 (async function() {
     var cameraKit = await bootstrapCameraKit({
-        apiToken: '<API_TOKEN>'
+        apiToken: 'eyJhbGciOiJIUzI1NiIsImtpZCI6IkNhbnZhc1MyU0hNQUNQcm9kIiwidHlwIjoiSldUIn0.eyJhdWQiOiJjYW52YXMtY2FudmFzYXBpIiwiaXNzIjoiY2FudmFzLXMyc3Rva2VuIiwibmJmIjoxNzEwNzM5NjY5LCJzdWIiOiI0ZWQ1MDVmMS0zNWEyLTQyZmMtOTkxYi1lMTljYmRhNjM3MmV-U1RBR0lOR340MjQwZjdhNi04ODJkLTQ0ODYtYTg4Yi1jZGEyYjcwNWUxMWEifQ.BPj2wsv2jCwEnmO3-BqwY4q73Mv2HWU3c6QrOZATNag'
     })
     const session = await cameraKit.createSession();
     document.getElementById('canvas').replaceWith(session.output.live)
 
-    const { lenses } = await cameraKit.lensRepository.loadLensGroups(['<GROUP_ID>'])
+    const { lenses } = await cameraKit.lensRepository.loadLensGroups(['771becad-5c50-4c2a-80b7-ac824a6922da'])
 
     session.applyLens(lenses[0])
-    let mediaStream = await navigator.mediaDevices.getUserMedia({video:true});
+    let mediaStream = await navigator.mediaDevices.getUserMedia({video:{facingMode:'environment'}});
 
     const source = createMediaStreamSource(mediaStream, {
-        transform: Transform2D.MirrorX,
-        cameraType:'front'
+        cameraType:'back'
     })
 
     await session.setSource(source)
